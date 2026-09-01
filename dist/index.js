919de4426a6d9df2c01c4706df1ece8399058530
Bola Nabil <bolanabil404@gmail.com>
2026-09-01 17:23:47 +0300

    compiler convert typescript code to javascript

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let personName = "bola";
+let age = 25;
+let isChecked = true;
+let country = "Egypt";
+let numberOfPeople = 30;
+//# sourceMappingURL=index.js.map
